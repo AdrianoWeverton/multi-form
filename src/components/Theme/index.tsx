972481346244402e0b8d27e0018cc489874defd5
Adriano Weverton <adriano.w.lara@gmail.com>
2022-03-27 14:53:01 -0300

    Finalizando o projeto.

---
+++ src/components/Theme/index.tsx
@@ -0,0 +1,51 @@
+import { ReactNode } from 'react';
+import { Header } from '../Header';
+import { SideBarItem } from '../SideBarItem';
+import { useForm } from '../../contexts/FormContext';
+import * as C from './styles';
+
+type Props = {
+    children: ReactNode;
+}
+
+export const Theme = ({children}: Props) => {
+    const {state} = useForm();
+
+    return (
+        <C.Container>
+            <C.Area>
+                <Header />
+
+                <C.Steps>
+                    <C.SideBar>
+                        <SideBarItem 
+                            title='Pessoal'
+                            description='Se identifique'
+                            icon='profile'
+                            path='/'
+                            active={state.currentStep === 1}
+                        />
+
+                        <SideBarItem 
+                            title='Profissional'
+                            description='Seu Nível'
+                            icon='book'
+                            path='/step2'
+                            active={state.currentStep === 2}
+                        />
+
+                        <SideBarItem 
+                            title='Contatos'
+                            description='Como te encontrar'
+                            icon='mail'
+                            path='/step3'
+                            active={state.currentStep === 3}                        />
+                    </C.SideBar>
+                    <C.Page>
+                        {children}
+                    </C.Page>
+                </C.Steps>
+            </C.Area>
+        </C.Container>
+    );
+}
---
+++ src/pages/FormStep1/index.tsx
@@ -0,0 +1,56 @@
+import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../components/Theme';
+import { useNavigate } from 'react-router-dom'; 
+import { ChangeEvent, useEffect } from 'react';
+
+export const FormStep1 = () => {
+    const history = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1,
+        })
+    }, []);
+
+    const handleNextStep = () => {
+        if(state.name !== '') {
+            history('/step2');     
+        } else{
+            alert("Preencha os dados!");
+        }
+    };
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        });
+    };
+
+    return(
+        <Theme>
+            <C.Container>
+                <p>Passo 1/3 - {state.currentStep}</p>
+                <h1>Vamos Começar com o seu nome</h1>
+                <p>Preencha o campo abaixo com o seu campo completo.</p>
+
+                <hr />
+
+                <label>
+                    Seu Nome Completo
+                    <input 
+                        type='text'
+                        autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
+                    />
+                </label>
+
+                <button onClick={handleNextStep} >Próximo</button>
+
+            </C.Container>
+        </Theme>
+    )
+}
